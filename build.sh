6ad4eadcbd60199c2bfa0cfc963b36ac039af9a6
Chokladmunk <jakob.hok@gmail.com>
2017-11-12 11:56:43 +0100

    Added build file and updated main.pdf

---
+++ build.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+# Function that silence warnings
+function sil {
+  $1 &>/dev/null
+}
+
+# 1. Fetches alias list
+shopt -s expand_aliases
+sil source ~/.bash_profiles
+sil source ~/.bashrc
+
+# Checks if 'gs' is set as alias
+function git_status {
+  echo -e "**********Git Status..."
+  if alias gs &>/dev/null; then
+    gs
+  else
+    git status
+  fi
+}
+
+echo -e "**********Compiling to pdf...\n"
+# 2. Compiles tex-files to pdf
+# pdflatex *.tex
+echo -e "\n**********Done compiling.\n"
+
+# 3. Executes the function git_status
+git_status
+
+# 4. Adds the tex- and pdf-files 
+git add *.tex *.pdf
+echo -e "\n**********Added tex- and pdf-files to git.\n"
+
+# 5. Executes the function git_status
+git_status
